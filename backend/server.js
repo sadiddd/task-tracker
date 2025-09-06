4c0fe4b8913646e36fcfc1a1e1f34d76f47a1bad
Sadid <sadidkarami@gmail.com>
2025-09-06 14:13:04 -0600

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from "cors"
+import path from "path"
 
 import tasksRoutes from './src/routes/tasksRoutes.js';
 import { connectDB } from './src/config/db.js';
@@ -8,16 +9,27 @@ import { connectDB } from './src/config/db.js';
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
+    origin: "http://localhost:5173",
+}))
+}
+
 
 app.use(express.json());
-app.use(cors({
-    // origin: "http://localhost:5173",
-}
-))
 
 app.use("/api/tasks", tasksRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend/dist","index.html"))
+})
+}
 
 connectDB().then(() => {
     app.listen(5001, () => {
